33a1ae9061091f20c73324673023a7b0829de389
KBandipo~ <keneebandipo@gmail.com>
2024-04-12 14:18:25 +0100

    Start the App section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 function App() {
-
-
   return (
     <>
-    <h1 className="font-bold undeline text-3xl">
-      Hello world!
-    </h1>
+      <h1 className="text-sm font-normal leading-6  underline md:text-base lg:text-[18px] ">
+        ToDo App
+      </h1>
+      <h2 className="text-bold bg-transparent text-right leading-5 text-orange-500 ">
+        Headings
+      </h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
