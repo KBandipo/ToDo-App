8dc8e8990d7c765968016589115dc14e44e0f0f4
KBandipo~ <keneebandipo@gmail.com>
2024-04-15 15:21:55 +0100

    Add inline style to the h1 in the Logo'jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,12 @@
 function Logo() {
-  return <h1 className="bg-[#f4a226] text-center text-[8rem]   ">My ToDo's</h1>;
+  return (
+    <h1
+      style={{ wordSpacing: "30px" }}
+      className="bg-[#f4a226] py-[2.4rem] text-center text-[8rem] font-normal uppercase tracking-[-5px] "
+    >
+      My ToDo's
+    </h1>
+  );
 }
 
 export default Logo;
